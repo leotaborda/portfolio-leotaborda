9e6856c99ea3e0bbdc9e9e718ae601b1138c81d2
leotaborda <leo.taboorda@gmail.com>
2026-06-09 00:41:38 -0300

    chore: adjustments

--- server.js
+++ server.js
@@ -4,6 +4,34 @@ const path = require("path");
 
 const app = express();
 
+app.get("/js/firebase-config.js", (req, res) => {
+    const config = {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID || "",
+    };
+
+    const missing = Object.entries(config)
+        .filter(([key, value]) => key !== "measurementId" && !value)
+        .map(([key]) => key);
+
+    if (missing.length > 0) {
+        const localConfigPath = path.join(__dirname, "public", "js", "firebase-config.js");
+        res.sendFile(localConfigPath, (err) => {
+            if (err) {
+                res.status(500).type("text/javascript").send(`console.error("Firebase env vars missing: ${missing.join(", ")}");`);
+            }
+        });
+        return;
+    }
+
+    res.type("text/javascript").send(`export const firebaseConfig = ${JSON.stringify(config, null, 2)};`);
+});
+
 app.use(express.static("public"));
 
 // Serve admin panel at /xpanel
